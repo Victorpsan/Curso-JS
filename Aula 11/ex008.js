527924a6de24a56210ca76da117539fe85ee9d46
Victor Pontes dos Santos <victorpsan@gmail.com>
2023-06-09 11:30:01 -0300

    Aula 11 - Condições (Parte 1)

---
+++ Aula 11/ex008.js
@@ -0,0 +1,9 @@
+console.log('TREINAMENTO DE CONDIçÕES!')
+console.log('Exemplo 1:')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Você foi multado porque ultrapassou a velocidade permitida`)
+}
+console.group(`Sempre use o cinto de segurança. Se beber não dirija!`)
+
---
+++ Aula 11/ex009.js
@@ -0,0 +1,10 @@
+console.log('TREINAMENTO DE CONDIçÕES!')
+
+console.log('Exemplo 2:')
+var país = 'Peru'
+console.log(`Vivendo em ${país}.`)
+if (país == 'Brasil') {
+    console.log(`Você é brasileiro`)
+} else {
+    console.log(`Você é estrangeiro`)
+}
