cc61b72ce5ade43b1aab334788fb74485466f1de
Victor Pontes dos Santos <victorpsan@gmail.com>
2023-06-13 15:42:52 -0300

    Aulas 13 e 14 - Repetições

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,12 @@
+var c = 1
+/*while (c <= 6) {
+    console.log('Tudo tudo?')
+    c ++
+}*/ 
+//estrutura de repetição com teste lógico no início
+
+do {
+    console.log(`Passo ${c}`)
+    c ++
+} while (c <= 9)
+//Estrutura de repetição com teste lógico no final
